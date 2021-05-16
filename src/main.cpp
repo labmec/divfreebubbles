73bbf163700423f08bd99250e7b11021228c8e1f
Nathan Shauer <nathan.sh@gmail.com>
2021-05-16 11:09:47 -0300

    First CMakeLists and empty main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#ifdef HAVE_CONFIG_H
+  #include <pz_config.h>
+#endif
+
+#include "TPZGenGrid2D.h"
+
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cstdio>
+
+//-------------------------------------------------------------------------------------------------
+//   __  __      _      _   _   _     
+//  |  \/  |    / \    | | | \ | |
+//  | |\/| |   / _ \   | | |  \| |
+//  | |  | |  / ___ \  | | | |\  |
+//  |_|  |_| /_/   \_\ |_| |_| \_|
+//-------------------------------------------------------------------------------------------------
+using namespace std;
+
+int main(int argc, char* argv[]){
+  
+  cout << "\nHello LabMeC!" << endl;
+
+  return 0;
+}
