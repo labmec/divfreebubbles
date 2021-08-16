ae9e2e23b3479e60968c52a859ab72c8c7c2967b
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-08-16 19:26:40 -0300

    better documentation on TPZCompElHCurlNoGrads

--- divfree/TPZCompElHCurlNoGrads.h
+++ divfree/TPZCompElHCurlNoGrads.h
@@ -5,7 +5,12 @@
 
 
 /**
-   @brief TPZCompElHCurlNoGrads<TSHAPE> is quite a fancy class.
+   @brief TPZCompElHCurlNoGrads<TSHAPE> generates a subset of HCurl functions
+   by removing functions needed to represent the high-order gradient fields.
+   
+   Upon treatment of the mesh (i.e., filtering some of the edges out of the system),
+   these elements can be used for generating an approximation space such that
+   dim(curl(phi)) = dim(phi).
 */
 template<class TSHAPE>
 class TPZCompElHCurlNoGrads  : public TPZCompElHCurlFull<TSHAPE> {
@@ -20,29 +25,38 @@ public:
    * @return number of shape functions
    */
   int NConnectShapeF(int connect, int order) const override;
-
+  //! Fills data.fMasterDirections, data.fVecShapeIndex and data.fShapeType.
   void InitMaterialData(TPZMaterialData &data) override;
-
+  //! Computes data.phi and data.curlphi to be used in the integration points.
   void ComputeRequiredData(TPZMaterialDataT<STATE> &data, TPZVec<REAL> &qsi) override{
     ComputeRequiredDataT(data,qsi);
   }
+  //! Computes data.phi and data.curlphi to be used in the integration points.
   void ComputeRequiredData(TPZMaterialDataT<CSTATE> &data, TPZVec<REAL> &qsi) override{
     ComputeRequiredDataT(data,qsi);
   }
-
+  //! Fills data.sol and data.curlsol.
   void ReallyComputeSolution(TPZMaterialDataT<STATE>& data) override{
     ReallyComputeSolutionT(data);
   }
+  //! Fills data.sol and data.curlsol.
   void ReallyComputeSolution(TPZMaterialDataT<CSTATE>& data) override{
     ReallyComputeSolutionT(data);
   }
 protected:
+  //! Adjusts the number of shape functions and block size of the connects.
   void AdjustConnects();
+  //! Combines/filters the HCurl functions from TPZCompElHCurlFull<TSHAPE>.
   void ComputeShape(TPZMaterialData &data, TPZFMatrix<REAL> &phiHCurl);
   template<int DIM>
+  /** 
+   * @brief Combines/filters the HCurl functions from TPZCompElHCurlFull<TSHAPE>
+   and compute their curl, filling data.curlphi.*/
   void ComputeCurl(TPZMaterialData &data);
+  //! Computes data.phi and data.curlphi to be used in the integration points.
   template<class TVar>
   void ComputeRequiredDataT(TPZMaterialDataT<TVar> &data, TPZVec<REAL> &qsi);
+  //! Fills data.sol and data.curlsol.
   template<class TVar>
   void ReallyComputeSolutionT(TPZMaterialDataT<TVar> &data);
   
