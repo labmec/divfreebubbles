30fbf9262d36ab29e3a364134ac191161fb0b1c7
jefersondossa <jwdfernandes@gmail.com>
2022-04-28 14:39:44 -0300

    dupl connects element working for 2 and 3D, iterative method and condensation of boundary DOFs

---
+++ test/dupl_connects.cpp
@@ -0,0 +1,417 @@
+/*
+  This unit test verifies if the hybridization and semi hybridization techniques are working
+  for any specified polynomial order and topology.
+  
+*/
+#include <catch2/catch.hpp>
+#include <TPZGeoMeshTools.h>
+#include "TPZHDivApproxSpaceCreator.h"
+#include "TPZKernelHdivUtils.h"
+#include "TPZAnalyticSolution.h"
+#include <TPZGmshReader.h>
+#include "TPZCompMeshTools.h"
+#include "pzlog.h"
+
+#include "pzshapequad.h"
+#include "pzshapetriang.h"
+#include "pzshapecube.h"
+#include "pzshapetetra.h"
+
+/** @brief Returns the name of the HDiv Family approximation space. */
+inline std::string MHDivFamily_Name(HDivFamily hdivfam)
+{
+	switch (hdivfam)
+	{
+		case HDivFamily::EHDivStandard:
+		{
+			return "EHDivStandard";
+		}
+		case HDivFamily::EHDivConstant:
+		{
+			return "EHDivConstant";
+		}
+		case HDivFamily::EHDivKernel:
+		{
+			return "EHDivKernel";
+		}
+		default:
+        {
+            return "HDivFamily not found!";
+        }
+    }
+    DebugStop();
+	return "";
+}
+
+/** @brief Returns the name of the Hybridization type. */
+inline std::string ApproxSpaceKernelHDiv_Name(TPZHDivApproxSpaceCreator<STATE>::MSpaceType approxSpace)
+{
+	switch (approxSpace)
+	{
+		case TPZHDivApproxSpaceCreator<STATE>::ENone:
+		{
+			return "ENone";
+		}
+		case TPZHDivApproxSpaceCreator<STATE>::EFullHybrid:
+		{
+			return "EFullHybrid";
+		}
+		case TPZHDivApproxSpaceCreator<STATE>::ESemiHybrid:
+		{
+			return "ESemiHybrid";
+		}
+		case TPZHDivApproxSpaceCreator<STATE>::EDuplicatedConnects:
+		{
+			return "EDuplicatedConnects";
+		}
+		default:
+        {
+            return "TPZHDivApproxSpaceCreator not found!";
+        }
+    }
+    DebugStop();
+	return "";
+}
+
+
+enum EMatid  {ENone, EDomain, EBoundary1, EBoundary2, EBoundary3, EBoundary4, EPont, EWrap, EIntface, EPressureHyb};
+
+/**
+   @brief Creates a geometric mesh with elements of a given type on a unit square or cube (depending on the mesh dimension).
+   @param[in] meshType element type to be created.
+   @param[in] nDivs Number of divisions (rows of elements) in x, y and z.
+   @param[in] volId Material identifier for the volumetric region.
+   @param[in] bcId Material identifier for the boundary.
+*/
+template<class tshape>
+TPZGeoMesh*
+CreateGeoMesh(TPZVec<int> &nDivs, EMatid volId, EMatid bcId);
+
+/**
+   @brief Reads the test mesh from gmsh
+   @param[in] file_name the .msh mesh file.
+*/
+template<class tshape>
+TPZGeoMesh*
+ReadMeshFromGmsh(std::string file_name);
+
+
+// The test function
+template<class tshape>
+void TestHybridization(const int &xdiv, const int &pOrder, HDivFamily &hdivfamily, TPZHDivApproxSpaceCreator<STATE>::MSpaceType &approxSpace);
+
+TEST_CASE("Hybridization test")
+{
+    const int xdiv = GENERATE(2);
+    const int pOrder = GENERATE(1);
+    // HDivFamily hdivfam = GENERATE(HDivFamily::EHDivConstant,HDivFamily::EHDivKernel);
+    // HDivFamily hdivfam = GENERATE(HDivFamily::EHDivKernel);
+    HDivFamily hdivfam = GENERATE(HDivFamily::EHDivConstant);
+    // HDivFamily hdivfam = GENERATE(HDivFamily::EHDivStandard,HDivFamily::EHDivConstant);
+    TPZHDivApproxSpaceCreator<STATE>::MSpaceType approxSpace = GENERATE(TPZHDivApproxSpaceCreator<STATE>::EDuplicatedConnects);
+    
+    // TestHybridization<pzshape::TPZShapeTriang>(xdiv,pOrder,hdivfam,approxSpace);
+    // TestHybridization<pzshape::TPZShapeQuad>(xdiv,pOrder,hdivfam,approxSpace); 
+    TestHybridization<pzshape::TPZShapeTetra>(xdiv,pOrder,hdivfam,approxSpace); 
+    // TestHybridization<pzshape::TPZShapeCube>(xdiv,pOrder,hdivfam,approxSpace);
+}
+
+//Analytical solution
+constexpr int solOrder{4};
+auto exactSol = [](const TPZVec<REAL> &loc,
+    TPZVec<STATE>&u,
+    TPZFMatrix<STATE>&gradU){
+    const auto &x=loc[0];
+    const auto &y=loc[1];
+    const auto &z=loc[2];
+
+    const auto &d = 1.; // distanc between injection and production wells
+    u[0]= x*x-y*y ;
+    gradU(0,0) = -2*x;
+    gradU(1,0) = 2.*y;
+    // gradU(2,0) = 0.;
+
+    // u[0] =  5. + 3. * x + 2. * y + 4. * x * y;
+    // gradU(0,0) = 3. + 4. * y;
+    // gradU(1,0) = 2. + 4. * x;
+    // gradU(2,0) = 0.;
+    
+
+    // REAL aux = 1./sinh(sqrt(2)*M_PI);
+    // u[0] = sin(M_PI*x)*sin(M_PI*y)*sinh(sqrt(2)*M_PI*z)*aux;
+    // gradU(0,0) = -M_PI*cos(M_PI*x)*sin(M_PI*y)*sinh(sqrt(2)*M_PI*z)*aux;
+    // gradU(1,0) = -M_PI*cos(M_PI*y)*sin(M_PI*x)*sinh(sqrt(2)*M_PI*z)*aux;
+    // gradU(2,0) = -sqrt(2)*M_PI*cosh(sqrt(2)*M_PI*z)*sin(M_PI*x)*sin(M_PI*y)*aux;
+
+    u[0] = x*x*x*y - y*y*y*x;
+    gradU(0,0) = (3.*x*x*y - y*y*y);
+    gradU(1,0) = (x*x*x - 3.*y*y*x);
+
+    u[0]= x*x - y*y;
+    gradU(0,0) = 2.*x;
+    gradU(1,0) = -2.*y;
+    // gradU(2,0) = -1;
+
+    // REAL aux = 1./sinh(sqrt(2)*M_PI);
+    // u[0] = sin(M_PI*x)*sin(M_PI*y)*sinh(sqrt(2)*M_PI*z)*aux;
+    // gradU(0,0) = M_PI*cos(M_PI*x)*sin(M_PI*y)*sinh(sqrt(2)*M_PI*z)*aux;
+    // gradU(1,0) = M_PI*cos(M_PI*y)*sin(M_PI*x)*sinh(sqrt(2)*M_PI*z)*aux;
+    // gradU(2,0) = sqrt(2)*M_PI*cosh(sqrt(2)*M_PI*z)*sin(M_PI*x)*sin(M_PI*y)*aux;
+
+    // u[0]= std::sin(M_PI*x)*std::sin(M_PI*y);
+    // gradU(0,0) = M_PI*cos(M_PI*x)*sin(M_PI*y);
+    // gradU(1,0) = M_PI*cos(M_PI*y)*sin(M_PI*x);
+
+    // REAL a1 = 1./4;
+    // REAL alpha = M_PI/2;
+    // u[0] = x*a1*cos(x*alpha)*cosh(y*alpha) + y*a1*sin(x*alpha)*sinh(y*alpha);
+    // gradU(0,0) = -a1*(cosh(alpha*y)*(cos(alpha*x) - alpha*x*sin(alpha*x)) + alpha*y*cos(alpha*x)*sinh(alpha*y));
+    // gradU(1,0) = -a1*(alpha*y*cosh(alpha*y)*sin(alpha*x) + (alpha*x*cos(alpha*x) + sin(alpha*x))*sinh(alpha*y));
+};
+
+template<class tshape>
+void TestHybridization(const int &xdiv, const int &pOrder, HDivFamily &hdivfamily, TPZHDivApproxSpaceCreator<STATE>::MSpaceType &approxSpace)
+{
+#ifdef PZ_LOG
+    TPZLogger::InitializePZLOG();
+#endif
+
+    std::cout << "\nTest Case: \nTopology = " << MElementType_Name(tshape::Type()) << 
+                 ", xdiv = " << xdiv << ", pOrder = " << pOrder << 
+                 ", Approximation space = " << MHDivFamily_Name(hdivfamily) <<
+                 ", Hybridization = " << ApproxSpaceKernelHDiv_Name(approxSpace) << "\n\n "; 
+    
+    int DIM = tshape::Dimension;
+    TPZVec<int> nDivs;
+
+    if (DIM == 2) nDivs = {xdiv,xdiv};
+    if (DIM == 3) nDivs = {xdiv,xdiv,xdiv};
+    
+    // Creates/import a geometric mesh
+    auto gmesh = CreateGeoMesh<tshape>(nDivs, EDomain, EBoundary1);
+    // auto gmesh = ReadMeshFromGmsh<tshape>("../mesh/1tetra.msh");
+
+    // Util for HDivKernel printing and solving
+    TPZKernelHdivUtils<STATE> util;
+
+    // Creates the approximation space generator
+    TPZHDivApproxSpaceCreator<STATE> createSpace(gmesh, approxSpace, hdivfamily);
+
+    //Insert here the BC material id's to be hybridized
+    std::set<int> matBCHybrid={};
+    std::set<int> matBCNeumann={};
+    std::set<int> matBCDirichlet={EBoundary1,EBoundary2,EBoundary3,EBoundary4};
+    std::set<int> matBCAll;
+    std::set_union(matBCNeumann.begin(),matBCNeumann.end(),matBCDirichlet.begin(),matBCDirichlet.end(),std::inserter(matBCAll, matBCAll.begin()));
+
+    //Setting material ids      
+    createSpace.fConfig.fDomain = EDomain;
+    createSpace.SetMaterialIds(EWrap,EPressureHyb,EIntface,EPont,matBCHybrid,matBCAll);
+    createSpace.SetPOrder(pOrder);
+    createSpace.Initialize();
+    util.PrintGeoMesh(gmesh);
+
+    //In the case of hybridized HDivConstant, we need 2 pressure meshes, so a total of 3. Otherwise, only 2 CompMeshes are needed 
+    int nMeshes = 4;
+    TPZVec<TPZCompMesh *> meshvector;
+    meshvector.Resize(nMeshes);
+
+    //Flux mesh
+    meshvector[0] = createSpace.CreateFluxCMesh();
+    std::string fluxFile = "FluxCMesh";
+    util.PrintCompMesh(meshvector[0],fluxFile);
+    // std::cout << "Flux mesh \n";
+    // util.PrintCMeshConnects(meshvector[0]);
+    
+    //Pressure mesh
+    meshvector[1]  = createSpace.CreatePressureCMesh();
+    std::string presFile = "PressureCMesh";
+    util.PrintCompMesh(meshvector[1],presFile);
+    // std::cout << "Pressure mesh \n";
+    // util.PrintCMeshConnects(meshvector[1]);
+
+    //G average mesh
+    meshvector[2] = createSpace.CreateConstantCmesh(gmesh,false);
+    std::string gavgFile = "GaverCMesh";
+    util.PrintCompMesh(meshvector[2],gavgFile);
+
+    //P average mesh
+    meshvector[3] = createSpace.CreateConstantCmesh(gmesh,true);
+    std::string pavgFile = "PaverCMesh";
+    util.PrintCompMesh(meshvector[3],pavgFile);
+
+    //Multiphysics mesh
+    auto * cmesh = createSpace.CreateMultiphysicsCMesh(meshvector,exactSol,matBCNeumann,matBCDirichlet);
+    // TPZCompMeshTools::CreatedCondensedElements(cmesh,true,false);
+    TPZCompMeshTools::CondenseElements(cmesh, 3, false);
+    std::string multFile = "MultiCMesh";
+    util.PrintCompMesh(cmesh,multFile);
+    std::cout << "Multi mesh \n";
+    util.PrintCMeshConnects(cmesh);
+
+    // Number of equations without condense elements
+    int nEquationsFull = cmesh->NEquations();
+    std::cout << "Number of equations = " << nEquationsFull << std::endl;
+
+    // Group and condense the elements
+    if (DIM == 2 && approxSpace == TPZHDivApproxSpaceCreator<STATE>::EDuplicatedConnects){
+        createSpace.CondenseDuplicatedConnects(cmesh);
+    }
+
+    //Number of condensed problem.
+    int nEquationsCondensed = cmesh->NEquations();
+
+    //Create analysis environment
+    TPZLinearAnalysis an(cmesh,false);
+    an.SetExact(exactSol,solOrder);
+
+    //Solve problem
+    if (approxSpace == TPZHDivApproxSpaceCreator<STATE>::EDuplicatedConnects){
+        util.SolveProblemMatRed(an, cmesh, matBCAll);
+    } else {
+        //Equation filter (spanning trees), true if 3D and HDivKernel 
+        bool filter = false;
+        if (DIM == 3 && hdivfamily == HDivFamily::EHDivKernel) filter = true;
+        createSpace.Solve(an, cmesh, true, filter);
+    }
+
+    //Print results
+    util.PrintResultsMultiphysics(meshvector,an,cmesh);
+
+    //Compute error
+    std::ofstream anPostProcessFile("postprocess.txt");
+    TPZManVector<REAL,5> error;
+    int64_t nelem = cmesh->NElements();
+    cmesh->LoadSolution(cmesh->Solution());
+    cmesh->ExpandSolution();
+    cmesh->ElementSolution().Redim(nelem, 5);
+    an.PostProcessError(error,false,anPostProcessFile);
+    
+    //Check error
+    REAL tolerance = 1.e-6;
+    std::cout << "ERROR[0] = " << std::scientific << std::setprecision(15) << error[0] << std::endl;
+    std::cout << "ERROR[1] = " << error[1] << std::endl;
+    std::cout << "ERROR[2] = " << error[2] << std::endl;
+    // std::cout << "ERROR[3] = " << error[3] << std::endl;
+    // std::cout << "ERROR[4] = " << error[4] << std::endl;
+    REQUIRE(error[1] < tolerance);
+
+    //Trying to design other criteria besides the approximation error
+    //Contar numero de equações condensadas = numero de arestas internas * porder
+    int nInternalEdges = 0;
+    switch (tshape::Type()){
+    case EQuadrilateral:
+        nInternalEdges = xdiv*(xdiv-1)*2;
+        break;
+    case ETriangle:
+        nInternalEdges = xdiv*(xdiv-1)*2 + xdiv*xdiv;
+        break;
+    
+    default:
+        break;
+    }
+    
+    // REQUIRE(nInternalEdges * pOrder == nEquationsCondensed);
+
+
+}
+
+//Create 
+template <class tshape>
+TPZGeoMesh*
+CreateGeoMesh(TPZVec<int> &nDivs, EMatid volId, EMatid bcId)
+{
+    
+    MMeshType meshType;
+    int dim = tshape::Dimension;
+
+    switch (tshape::Type())
+    {
+    case ETriangle:
+        meshType = MMeshType::ETriangular;
+        break;
+    case EQuadrilateral:
+        meshType = MMeshType::EQuadrilateral;
+        break;
+    case ETetraedro:
+        meshType = MMeshType::ETetrahedral;
+        break;
+    case ECube:
+        meshType = MMeshType::EHexahedral;
+        break;
+        case EPrisma:
+        meshType = MMeshType::EPrismatic;
+        break;
+    default:
+        DebugStop();
+    }
+
+    TPZManVector<REAL,3> minX = {0,0,0};
+    TPZManVector<REAL,3> maxX = {1,1,1};
+    int nMats = 2*dim+1;
+
+    //all bcs share the same id
+    constexpr bool createBoundEls{true};
+    TPZVec<int> matIds(nMats,bcId);
+    matIds[0] = volId;
+    matIds[1] = EBoundary1;
+    matIds[2] = EBoundary2;
+    matIds[3] = EBoundary3;
+    matIds[4] = EBoundary4;
+    
+    TPZGeoMesh* gmesh = TPZGeoMeshTools::CreateGeoMeshOnGrid(dim, minX, maxX,
+                        matIds, nDivs, meshType,createBoundEls);
+    // TPZGeoMesh* gmesh = TPZGeoMeshTools::CreateGeoMeshSingleEl(meshType,
+    //                     volId,createBoundEls, bcId);
+    
+    return gmesh;
+    
+}
+
+
+template <class tshape>
+TPZGeoMesh*
+ReadMeshFromGmsh(std::string file_name)
+{
+    //read mesh from gmsh
+    TPZGeoMesh *gmesh;
+    gmesh = new TPZGeoMesh();
+    {
+        TPZGmshReader reader;
+        // essa interface permite voce mapear os nomes dos physical groups para
+        // o matid que voce mesmo escolher
+        TPZManVector<std::map<std::string,int>,4> stringtoint(4);
+        stringtoint[3]["Domain"] = 1;
+        stringtoint[2]["Surfaces"] = 2;
+
+        reader.SetDimNamePhysical(stringtoint);
+        reader.GeometricGmshMesh(file_name,gmesh);
+    }
+
+    return gmesh;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
